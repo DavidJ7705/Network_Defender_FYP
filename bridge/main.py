5d5ba6e174fbfea8cbf1f7c1506fc3746c9cd342
david <G00419108@atu.ie>
2026-03-28 20:07:10 +0000

    initialised each class call. must build out functionality

--- bridge/main.py
+++ bridge/main.py
@@ -1,1 +1,20 @@
-hi
+from network_monitor import ContainerlabMonitor
+from graph_builder import ObservationGraphBuilder
+from agent_adapter import AgentAdapter
+from action_executor import ActionExecutor
+from red_agent import RedAgent
+from intrusion_detector import IntrusionDetector
+
+MAX_STEPS = 100
+
+monitor = ContainerlabMonitor()
+builder = ObservationGraphBuilder()
+adapter = AgentAdapter()
+executor = ActionExecutor()
+detector = IntrusionDetector
+
+state = monitor.get_network_state()
+servers, users, routers = builder.classify_node_type(state)
+containers = servers + users 
+
+red_agent = RedAgent(containers,decoys = executor._decoys)
